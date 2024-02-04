fc37c840348a4db748cf9ce01f9e6522ef79e2ab
Melika Shahraki <144850606+its-Melika@users.noreply.github.com>
2024-02-05 02:08:22 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,140 @@
+let iconCart = document.querySelector('.icon-cart');
+let cart = document.querySelector('.cart');
+let container = document.querySelector('.container');
+let close = document.querySelector('.close');
+
+iconCart.addEventListener('click', function () {
+    if (cart.style.right == '-100%') {
+        cart.style.right = '0';
+        container.style.transform = 'translateX(-400px)';
+    } else {
+        cart.style.right = '-100%';
+        container.style.transform = 'translateX(0)';
+    }
+})
+close.addEventListener('click', function () {
+    cart.style.right = '-100%';
+    container.style.transform = 'translateX(0)';
+})
+
+
+let products = null;
+
+fetch('product.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        addDataToHTML();
+    })
+
+
+function addDataToHTML() {
+
+    let listProductHTML = document.querySelector('.list-products');
+    listProductHTML.innerHTML = '';
+
+
+    if (products != null) {
+        products.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('item');
+            newProduct.innerHTML =
+                `<img src="${product.img}" alt="">
+            <h2>${product.name}</h2>
+            <div class="price">$${product.price}</div>
+            <button onclick="addCart(${product.id})">Add To Cart</button>`;
+
+            listProductHTML.appendChild(newProduct);
+
+        });
+    }
+}
+
+
+
+let listCart = [];
+function checkCart() {
+    var cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('listCart='));
+    if (cookieValue) {
+        listCart = JSON.parse(cookieValue.split('=')[1]);
+    } else {
+        listCart = [];
+    }
+}
+checkCart();
+
+
+function addCart($idProduct) {
+    let productsCopy = JSON.parse(JSON.stringify(products));
+
+
+    if (!listCart[$idProduct]) {
+        listCart[$idProduct] = productsCopy.filter(product => product.id == $idProduct)[0];
+        listCart[$idProduct].quantity = 1;
+    } else {
+
+        listCart[$idProduct].quantity++;
+    }
+    document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
+
+    addCartToHTML();
+}
+
+addCartToHTML();
+function addCartToHTML() {
+
+    let listCartHTML = document.querySelector('.listcart');
+    listCartHTML.innerHTML = '';
+
+    let totalHTML = document.querySelector('.total-quantity');
+    let totalQuantity = 0;
+
+    if (listCart) {
+        listCart.forEach(product => {
+            if (product) {
+                let newCart = document.createElement('div');
+                newCart.classList.add('item');
+                newCart.innerHTML =
+                    `<img src="${product.img}">
+                    <div class="content">
+                        <div class="name">${product.name}</div>
+                        <div class="price">$${product.price} / 1 product</div>
+                    </div>
+                    <div class="quantity">
+                        <button onclick="changeQuantity(${product.id}, '-')">-</button>
+                        <span class="value">${product.quantity}</span>
+                        <button onclick="changeQuantity(${product.id}, '+')">+</button>
+                    </div>`;
+                listCartHTML.appendChild(newCart);
+                totalQuantity = totalQuantity + product.quantity;
+            }
+        })
+    }
+    totalHTML.innerText = totalQuantity;
+}
+
+
+function changeQuantity($idProduct, $type) {
+    switch ($type) {
+        case '+':
+            listCart[$idProduct].quantity++;
+            break;
+        case '-':
+            listCart[$idProduct].quantity--;
+
+
+            if (listCart[$idProduct].quantity <= 0) {
+                delete listCart[$idProduct];
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    document.cookie = "listcart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
+
+    addCartToHTML();
+}
